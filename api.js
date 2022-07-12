7442fecbc686456ff192379d889b504690a98f6d
Gabriel Oliva <gfpoliva@gmail.com>
2022-07-12 16:46:20 -0300

    chamada à api exchange rate. Closes #1

---
+++ api.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+const BASE_URL = 'https://api.exchangerate.host';
+const LATEST_ENDPOINT = '/latest';
+
+const buildUrlBasedOnCurrency = (currency) => {
+  return `${BASE_URL}${LATEST_ENDPOINT}/?base=${currency}`;
+}
+
+const fetchCurrency = async (currency) => {
+  const endpoint = buildUrlBasedOnCurrency(currency);
+
+  try {
+    const response = await fetch(endpoint);
+    const json = await response.json();
+
+    return json
+  } catch (error) {
+    throw error;
+  }
+
+}
